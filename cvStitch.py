e451f7bbae81b432c92d986d3e42492123872cd5
Matan Weissbuch <matan2050@gmail.com>
2022-01-22 23:43:33 +0200

    homography estimation and stitch infra

---
+++ cvStitch.py
@@ -0,0 +1,44 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+def ConvertImageColorspace(img1, img2):
+    img1_cvt = img1
+    img2_cvt = img2
+    
+    if img1.ndim > 2 and img1.shape[2] == 3:
+        img1_cvt = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    if img2.ndim > 2 and img2.shape[2] == 3:
+        img2_cvt = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+        
+    return img1_cvt, img2_cvt
+
+def GenerateKeypointsDescriptorSIFT(img):
+    sift = cv2.xfeatures2d.SIFT_create()
+    kp, des = sift.detectAndCompute(img, None)
+    return kp, des
+
+def MatchDescriptorsSIFT(des1, des2, maximal_dist):
+    matcher = cv2.BFMatcher()
+    matches = matcher.knnMatch(des1, des2, k = 2)
+    
+    good = []
+    for m in matches:
+        if (m[0].distance < maximal_dist*m[1].distance):
+            good.append(m)
+    matches = np.asanyarray(good)
+    return matches
+    
+def GenerateHomographyTform(kp1, kp2, matches, minimal_matches, maximal_dist):
+    if len(matches[:,0]) < minimal_matches:
+        raise AssertionError('not enough matches')
+        
+    src = np.float32([kp1[m.queryIdx].pt for m in matches[:,0]]).reshape(-1,1,2)
+    dst = np.float32([kp2[m.trainIdx].pt for m in matches[:,0]]).reshape(-1,1,2)
+    
+    H, mask = cv2.findHomography(src, dst, cv2.RANSAC, maximal_dist)
+    return H
+    
+def Stitch(img1, img2, H):
+    
+    
